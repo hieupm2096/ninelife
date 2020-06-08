8437a23b645cfd7dc2902185c75a65421333a0c4
Hieu Pham <hieupm2096@gmail.com>
2020-06-08 22:49:45 +0700

    Initial Commit

---
+++ ninelife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ninelife
+//
+//  Created by Hieu Pham on 6/8/20.
+//  Copyright © 2020 Hieu Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
